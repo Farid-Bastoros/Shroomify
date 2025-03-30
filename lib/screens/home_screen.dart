caf8ebb5a0037a01ff422c9d4c5a7b3c6c997865
Farid-Bastoros <farid.sameh211@gmail.com>
2025-03-30 19:34:14 -0400

    Restructured the home screen for a more professional look. Added a bottom navigation bar and implemented sliding text to display mushroom facts.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,26 +1,70 @@
 import 'package:flutter/material.dart';
-//import '/screens/TextIdentification.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:marquee/marquee.dart';
 import '/screens/MicroImageIdentification.dart';
 
-
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
-  
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool showMushroomOfTheDay = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Shroomify')),
+      // appBar: AppBar(
+      //   title: Text(
+      //     'Shroomify',
+      //     style: GoogleFonts.poppins(
+      //       fontSize: 24,
+      //       fontWeight: FontWeight.bold,
+      //       color: Colors.white,
+      //     ),
+      //   ),
+      //   backgroundColor: Colors.deepPurpleAccent,
+      //   centerTitle: true,
+      //   elevation: 5,
+      // ),
+      appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Icon(
+              //   Icons.local_florist, 
+              //   color: Colors.white,
+              //   size: 28,
+              // ),
+              SizedBox(width: 8),
+              Text(
+                '🍄 Shroomify 🔍',
+                style: GoogleFonts.poppins(
+                  fontSize: 28,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          backgroundColor: Colors.deepPurpleAccent,
+          centerTitle: true,
+          elevation: 5,
+        ),
+
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             colors: [
-              Color.fromARGB(255, 19, 118, 74),
-              Color.fromARGB(255, 219, 37, 161),
+              Color(0xFF130F40),
+              Color(0xFF574B90),
+              Color(0xFF9F44D3),
             ],
-            // begin: Alignment.topLeft,
-            begin: Alignment(0.15, 0),
-            end: Alignment.bottomRight,
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
           ),
         ),
         child: Stack(
@@ -29,71 +73,210 @@ class HomeScreen extends StatelessWidget {
               top: 0,
               left: 0,
               right: 0,
-              child: Container(
-                height: 200,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/mush_cover.jpeg'),
-                    fit: BoxFit.cover,
+              child: FadeIn(
+                duration: Duration(seconds: 2),
+                child: Container(
+                  height: 220,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(30),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.5),
+                        blurRadius: 10,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/mush_cover.jpeg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 250,
+              left: 0,
+              right: 0,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: SizedBox(
+                  height: 40,
+                  child: Marquee(
+                    text: 
+                      ' Did you know? Some mushrooms, like Panellus stipticus, glow in the dark! 🌟  '
+                      ' Trivia: Over 10,000 types of mushrooms exist, ranging from edible to toxic! 🍄  '
+                      ' Fun fact: Mushrooms are genetically closer to animals than plants! 🧬  '
+                      ' Amazing: The largest living organism on Earth is a mushroom, spanning over 2,385 acres! 🌍  '
+                      ' Eco-friendly: Mushrooms help clean the environment by breaking down pollutants! 🌱  '
+                      ' Hydrating: Mushrooms are 90% water, making them light and easy to cook! 💧  '
+                      ' Mind-bending: Some mushrooms, like Psilocybe cubensis, have psychedelic effects! 🌈  '
+                      ' Ancient wisdom: Medicinal mushrooms have been used for thousands of years in traditional medicine! 💊  '
+                      ' Underground network: Mushrooms create a vast "Wood Wide Web," helping plants share resources! 🌐  '
+                      ' Nutritious: Edible mushrooms are packed with vitamins, minerals, and antioxidants! 🥗  ',
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                    scrollAxis: Axis.horizontal,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    blankSpace: 20.0,
+                    velocity: 50.0,
+                    pauseAfterRound: Duration(seconds: 1),
                   ),
                 ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(top: 200.0),
+              padding: const EdgeInsets.only(top: 250.0),
               child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        // Navigator.push(
-                        //   context,
-                        //   MaterialPageRoute(builder: (context) => TextIdentification()),
-                        // );
-                        },
-                      style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                        textStyle: const TextStyle(fontSize: 18),
+                child: BounceInUp(
+                  duration: Duration(seconds: 2),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Microimageidentification()),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 18),
+                      textStyle: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.bold),
+                      backgroundColor: Colors.orangeAccent,
+                      foregroundColor: Colors.white,
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
                       ),
-                      child: const Text('UNAVAILABLE'),
+                      shadowColor: Colors.black.withOpacity(0.5),
                     ),
-                    SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Navigator.push(
-                        //   context
-                        //   // MaterialPageRoute(builder: (context) => Macroimageidentification()),
-                        // );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        
-                        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                        textStyle: const TextStyle(fontSize: 18),
-                      ),
-                      child: const Text('UNAVAILABLE'),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.search, size: 26),
+                        SizedBox(width: 10),
+                        Text('Identify Mushroom'),
+                      ],
                     ),
-                    SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: () {
-                        
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Microimageidentification()),
-                        );},
-                      style: ElevatedButton.styleFrom(
-                        
-                        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                        textStyle: const TextStyle(fontSize: 18),
-                      ),
-                      child: const Text('Identify Mushroom'),
-                    ),
-                  ],
+                  ),
                 ),
               ),
             ),
+          
+            if (showMushroomOfTheDay)
+            Positioned(
+              top: 380,
+              left: 20,
+              right: 20,
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                color: Colors.white.withOpacity(0.9),
+                shadowColor: Colors.deepPurpleAccent.withOpacity(0.2),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Mushroom of the Day',
+                            style: GoogleFonts.poppins(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.deepPurpleAccent,
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.close, color: Colors.deepPurpleAccent),
+                            onPressed: () {
+                              setState(() {
+                                showMushroomOfTheDay = false;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 15),
+                      Text(
+                        'Panellus Stipticus: The Bioluminescent Mushroom!',
+                        style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black87,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        'Did you know that this mushroom glows in the dark? Perfect for bioluminescent enthusiasts!',
+                        style: GoogleFonts.poppins(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      ElevatedButton(
+                        onPressed: () {
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.deepPurpleAccent,
+                          foregroundColor: Colors.white,
+                          padding: EdgeInsets.symmetric(vertical: 12),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          elevation: 5,
+                        ),
+                        child: Text(
+                          'Learn More',
+                          style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+            ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.deepPurpleAccent,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white70,
+        showUnselectedLabels: true,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'My Mushrooms',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.restaurant_menu),
+            label: 'My Recipes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Account',
+          ),
+        ],
       ),
     );
   }
